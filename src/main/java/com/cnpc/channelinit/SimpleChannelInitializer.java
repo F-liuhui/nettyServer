d4195449c19d280b1ac11396c9f2b9e271236f2f
liuhui <liuhui200888@163.com>
2018-11-26 10:54:24 +0800

    修改包的位置

--- src/main/java/com/cnpc/channelinit/SimpleChannelInitializer.java
+++ src/main/java/com/cnpc/channelinit/SimpleChannelInitializer.java
@@ -1,5 +1,6 @@
-package com.cnpc.handler;
+package com.cnpc.channelinit;
 
+import com.cnpc.handler.SimpleServerHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
@@ -18,7 +19,6 @@ public class SimpleChannelInitializer extends ChannelInitializer<SocketChannel> 
     protected void initChannel(SocketChannel channel) throws Exception {
         //得到一个通道
         ChannelPipeline pipeline= channel.pipeline();
-
         //帧解码器 Delimiters.lineDelimiter()表示以\n结尾表示一个完整的消息，8192表示流长度。
         pipeline.addLast("framer", new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
 
